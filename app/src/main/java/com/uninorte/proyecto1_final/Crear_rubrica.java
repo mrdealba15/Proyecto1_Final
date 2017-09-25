143acdf89e2b60608b9cf25aed8e457499577cf3
Visitante <v>
2017-09-25 12:37:21 -0500

    UI

---
+++ app/src/main/java/com/uninorte/proyecto1_final/Crear_rubrica.java
@@ -0,0 +1,13 @@
+package com.uninorte.proyecto1_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Crear_rubrica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_rubrica);
+    }
+}
