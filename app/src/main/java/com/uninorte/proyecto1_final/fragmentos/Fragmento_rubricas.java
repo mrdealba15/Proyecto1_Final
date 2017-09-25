a23b7f45fd25e12bb23c865b39d0e7902c01e4fb
William Llanos <williama.llanos@hotmail.com>
2017-09-25 13:54:34 -0500

    Crear Rubrica

--- app/src/main/java/com/uninorte/proyecto1_final/fragmentos/Fragmento_rubricas.java
+++ app/src/main/java/com/uninorte/proyecto1_final/fragmentos/Fragmento_rubricas.java
@@ -1,5 +1,6 @@
 package com.uninorte.proyecto1_final.fragmentos;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -10,12 +11,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 import com.uninorte.proyecto1_final.MainActivity;
 import com.uninorte.proyecto1_final.R;
 import com.uninorte.proyecto1_final.adaptadores.Adaptador_cursos;
 import com.uninorte.proyecto1_final.adaptadores.Adaptador_rubricas;
+import com.uninorte.proyecto1_final.modelos.Categoria;
+import com.uninorte.proyecto1_final.modelos.Elemento;
 import com.uninorte.proyecto1_final.modelos.Rubrica;
 
 import java.util.List;
@@ -51,8 +55,46 @@ public class Fragmento_rubricas extends Fragment {
             @Override
             public void onClick (View view){
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setView(R.layout.activity_crear_rubrica);
-                builder.setPositiveButton("Crear",null);
+                View mView = getLayoutInflater().inflate(R.layout.activity_crear_rubrica, null);
+
+                final EditText etNombreRubrica = mView.findViewById(R.id.editText);
+                final EditText etNombreCategoria = mView.findViewById(R.id.editText16);
+                final EditText etPesoCategoria = mView.findViewById(R.id.editText10);
+                final EditText etNombreElemento = mView.findViewById(R.id.editText18);
+                final EditText etPesoElemento = mView.findViewById(R.id.editText25);
+                final EditText etL1 = mView.findViewById(R.id.editText17);
+                final EditText etL2 = mView.findViewById(R.id.editText19);
+                final EditText etL3 = mView.findViewById(R.id.editText20);
+                final EditText etL4 = mView.findViewById(R.id.editText21);
+
+                builder.setView(mView);
+                builder.setPositiveButton("Crear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Rubrica r = new Rubrica();
+                        r.setName(etNombreRubrica.getText().toString());
+                        r.save();
+
+                        Categoria c = new Categoria();
+                        c.setName(etNombreCategoria.getText().toString());
+                        c.setPeso(Float.parseFloat(etPesoCategoria.getText().toString()));
+                        c.setRubrica(r);
+                        c.save();
+
+                        Elemento e = new Elemento();
+                        e.setName(etNombreElemento.getText().toString());
+                        e.setPeso(Float.parseFloat(etPesoElemento.getText().toString()));
+                        e.setCategoria(c);
+                        e.setL1(etL1.getText().toString());
+                        e.setL2(etL2.getText().toString());
+                        e.setL3(etL3.getText().toString());
+                        e.setL4(etL4.getText().toString());
+                        e.save();
+
+                        adaptador.addRubrica(r);
+                    }
+                });
+                builder.setNegativeButton("Cancelar", null);
                 AlertDialog dialog = builder.create();
                 dialog.show();
             }
